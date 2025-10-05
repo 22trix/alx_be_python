503e8acf1635319b1e7efbcdc474ec48d3419b23
Goodness <goodnessojonuba@gmail.com>
2025-10-05 19:46:06 +0100

    Division fix

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -27,7 +27,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertAlmostEqual(self.calc.multiply(2.5, 2), 5.0)
 
     # --- Required: test_divide ---
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10, 5), 2)
         self.assertAlmostEqual(self.calc.divide(7, 2), 3.5)
         self.assertEqual(self.calc.divide(-9, -3), 3)
